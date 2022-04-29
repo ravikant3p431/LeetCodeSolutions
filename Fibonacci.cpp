8998f6bd7f465e09e447f869de41d7532a2f5393
ravikant3p431 <ravikant3p431@gmail.com>
2022-04-29 10:57:15 +0530

    Create Fibonacci.cpp

---
+++ Fibonacci.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int DPSolution(int n);
+    int RecursiveSolution(int n);
+    int RecursiveSolutionWithMemoization(int n);
+    int fib(int n) {
+       // return RecursiveSolutionWithMemoization(n);
+       // return RecursiveSolution(n);
+        return DPSolution(n);
+    }
+};
+int Solution::RecursiveSolutionWithMemoization(int n)
+{
+    vector<int> dp(n+1, -1);
+    if(n <= 1)
+        dp[n] = n;
+    if(dp[n] != -1)
+        return dp[n];
+    return dp[n] = RecursiveSolution(n-1) +RecursiveSolution(n-2); 
+
+}
+int Solution:: RecursiveSolution(int n)
+{
+    if(n <= 1)
+        return n;
+    return RecursiveSolution(n-1) +RecursiveSolution(n-2); 
+}
+int Solution::DPSolution(int n)
+{//int dp[n+1];
+        if(n <= 1)
+            return n;
+        int dp1 = 0 ; //optimizing the Space
+        int dp2 = 1;
+        int current = 0 ;
+        for(int i = 2 ; i <= n ; i++)
+        {
+            current = dp1 + dp2;
+            dp1 = dp2;
+            dp2= current;
+        } //dp[i] = dp[i-1] + dp[i-2];
+       // return dp[n+1];
+        return current;
+    
+}
